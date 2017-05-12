c6ad421a2ded4d6b9459f2780649f5d849727d40
pmaliy <pm@nxc.no>
2017-05-12 18:31:56 +0300

    Create script.js

---
+++ dashboard/js/script.js
@@ -0,0 +1,8 @@
+/*
+$.ajax({
+  url: 'https://sandbox-4bd39.firebaseio.com/messages.json',
+  method: 'post',
+  data: '{"user": "pm", "message": "omg it works", "timestamp": {".sv": "timestamp"}}',
+  success: data => console.log(data)
+})
+*/
