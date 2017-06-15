2ce5361555446bbf0a91841f02463415f79b45c8
pmaliy <pm@nxc.no>
2017-06-15 17:40:54 +0300

    Create datatable.js

--- datatable/js/datatable.js
+++ datatable/js/datatable.js
@@ -1,9 +1,13 @@
 (function() {
   const
-    HIDDEN_CLASSNAME = 'hidden';
+    HIDDEN_CLASSNAME = 'hidden',
+    VALUE_CLASSNAME = 'value',
+    INPUT_CLASSNAME = 'input';
   
+  let state = {};
+
   // DOM Element decorator
-  const enhanceElement = (element) => {
+  const enhanceElement = (element, data) => {
     return Object.assign(element, {
       hasClass(className) {
         return this.className.indexOf(className || null) >= 0;
@@ -26,100 +30,221 @@
         this.innerHTML = str;
       }
     });
-  }
+  };
   
   // DOM event handlers
   
   
-  // DOM elements generators
-  const generateCell = (model, head = true) => {
-    let cellElement = document.createElement(head ? 'td' : 'th');
-    
-    if (!head) {
-      cellElement.innerHTML = model.value.split('_').join(' ');
-      return cellElement;
+  // generators
+  const generateCell = (data) => {
+    let
+      td = document.createElement('td'),
+      div = document.createElement('div'),
+      val = enhanceElement(document.createElement('div')),
+      input = enhanceElement(document.createElement('input'));
+
+    div.className = 'cell';
+
+    val.className = VALUE_CLASSNAME;
+    val.innerHTML = data.value;
+
+    input.className = INPUT_CLASSNAME + ' ' + HIDDEN_CLASSNAME;
+    input.setAttribute('type', 'text');
+    input.setAttribute('value', data.value);
+
+    td.appendChild(div);
+    div.appendChild(val);
+    div.appendChild(input);
+
+    val.addEventListener('dblclick', e => {
+      input.show();
+      input.focus();
+    });
+
+    input.addEventListener('focus', e => {
+      input.value = input.value;
+    });
+
+    input.addEventListener('keyup', e => {
+      val.html(input.value);
+
+      // Enter key
+      if ((e.keyCode || e.which) == 13) {
+        input.blur();
+        input.hide();
+        data.value = input.value;
+        // updating textarea with json
+        updateTextarea();
+      }
+    });
+
+    return {
+      el: td,
+      div,
+      input
     }
-    
-    let inputElement = enhanceElement(document.createElement('input'));
-    inputElement.className = 'hidden';
-    inputElement.setAttribute('type', 'text');
-    inputElement.setAttribute('value', model.value);
-    
-    let valueElement = enhanceElement(document.createElement('div'));
-    valueElement.className = 'value';
-    valueElement.innerHTML = model.value;
-    
-    let cellInnerElement = document.createElement('div');
-    cellInnerElement.className = 'cell';
-    cellInnerElement.appendChild(valueElement);
-    cellInnerElement.appendChild(inputElement);
-    
-    cellElement.appendChild(cellInnerElement);
-    
-    model.inputElement = inputElement;
-    model.valueElement = valueElement;
-    
-    return cellElement;
   };
-  
-  const generateRow = (model, head = true) => {
+
+  const generateRow = (data) => {
     let
-      trElement = document.createElement('tr');
-    
-    Object.keys(model).map(key => {
-      trElement.appendChild(generateCell(model[key], head));
-    });
-    
-    return trElement;
+      tr = document.createElement('tr'),
+      cell;
+
+    return {
+      el: tr,
+      cells: Object.keys(data).map(key => {
+        cell = generateCell(data[key]);
+        tr.appendChild(cell.el);
+
+        return cell;
+      })
+    }
   };
-  
-  const generateHead = (collection) => {
-    let data = Object.keys(collection[0]).map(key => ({ value: key }));
-    
-    return generateRow(data, false);
-  };
-  
-  const generateTable = (collection) => {
+
+  const generateTbody = (data) => {
     let
-      tableElement = document.createElement('table');
-    
-    tableElement.appendChild(generateHead(collection));
-    
-    collection.map(item => {
-      tableElement.appendChild(generateRow(item));
-    });
-    
-    return tableElement;
+      tbody = document.createElement('tbody'),
+      row;
+
+    return {
+      el: tbody,
+      rows: data.map(rowData => {
+        row = generateRow(rowData);
+        tbody.appendChild(row.el);
+
+        return row;
+      })
+    }
   };
-  
-  // renderer
-  const outputTable = (element, data) => {
-    element.innerHTML = '';
-    element.appendChild(generateTable(data));
+
+  const generateThead = (data) => {
+    let
+      thead = document.createElement('thead'),
+      tr = document.createElement('tr'),
+      th,
+      row = {
+        el: tr,
+        cells: Object.keys(data[0]).map(key => {
+          th = document.createElement('th');
+          th.innerHTML = key.split('_').join(' ');
+          tr.appendChild(th);
+
+          return th;
+        })
+      };
+
+    thead.appendChild(row.el);
+
+    return {
+      el: thead,
+      row: row
+    }
   };
-  
+
+  const generateTable = (data) => {
+    let
+      table = document.createElement('table'),
+      thead = generateThead(data),
+      tbody = generateTbody(data);
+
+    table.appendChild(thead.el);
+    table.appendChild(tbody.el);
+
+    return {
+      el: table,
+      thead,
+      tbody
+    }
+  };
+
+  generateTextarea = (data) => {
+    let textarea = document.createElement('textarea');
+
+    textarea.className = 'output-json';
+    textarea.setAttribute('rows', 10);
+
+    return {
+      el: textarea
+    }
+  };
+
+  // json parser
+  const generateData = (source) => {
+    return source.map(
+      ({ name, height, mass, hair_color, skin_color, eye_color, birth_year }) =>
+      ({
+        name: {
+          type: 'string',
+          value: name,
+          nullValue: ''
+        },
+        height: {
+          type: 'numeric',
+          value: height,
+          nullValue: 0
+        },
+        mass: {
+          type: 'numeric',
+          value: mass,
+          nullValue: 0
+        },
+        hair_color: {
+          type: 'string',
+          value: hair_color,
+          nullValue: 'n/a'
+        },
+        skin_color: {
+          type: 'string',
+          value: skin_color,
+          nullValue: ''
+        },
+        eye_color: {
+          type: 'string',
+          value: eye_color,
+          nullValue: ''
+        },
+        birth_year: {
+          type: 'numeric',
+          value: birth_year,
+          nullValue: 'unknown'
+        }
+      })
+    );
+  };
+
+  // outputters
+  const outputData = (element) => {
+    element.appendChild(state.textarea.el);
+  };
+
+  const outputTable = (element) => {
+    element.appendChild(state.table.el);
+  };
+
+  const updateTextarea = () => {
+    state.textarea.el.innerHTML = JSON.stringify(state.data.map(item => {
+      return Object.keys(item).reduce((obj, key) => {
+        obj[key] = item[key].value;
+        return obj;
+      }, {});
+    }));
+  };
+
   // core
-  const outputData = (selector, data) => {
+  const dataTable = (selector, source) => {
     const containerElement = document.querySelector(selector);
     
-    if (!containerElement || !data.length) return;
-    
-    const collection = data.reduce((arr, item) => {
-      arr.push({
-        name: { value: item.name },
-        height: { value: item.height },
-        mass: { value: item.mass },
-        hair_color: { value: item.hair_color },
-        skin_color: { value: item.skin_color },
-        eye_color: { value: item.eye_color },
-        birth_year: { value: item.birth_year }
-      });
-      
-      return arr;
-    }, []);
-    
-    outputTable(containerElement, collection);
+    if (!containerElement || !source.length) return;
+
+    state.data = generateData(source);
+    state.table = generateTable(state.data);
+    state.textarea = generateTextarea(state.data);
+
+    outputTable(containerElement);
+    outputData(containerElement);
+
+    updateTextarea();
   };
   
-  window.outputData = outputData;
+  window.dataTable = dataTable;
 })();
